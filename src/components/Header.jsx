d268e72b67f038e46b276d4adff9d0470c8b360f
Ilya Mur <ilyamur@hotmail.com>
2021-09-07 02:56:44 +0300

    fix header link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export function Header() {
   return (
     <nav className="green darken-1">
       <div className="nav-wrapper">
-        <a href="./react_shop/" className="brand-logo">
+        <a href="#!" className="brand-logo">
           React Toy-Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
